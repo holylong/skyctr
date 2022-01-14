9e1648a256bd829ce9f019a499557ab0bacae8be
holylong <mrhlingchen@163.com>
2022-01-14 12:18:41 +0800

    skyctr

--- src/ConvertorStruct.h
+++ src/ConvertorStruct.h
@@ -10,8 +10,27 @@
  * C++构建工具先关命令与定义
  */
 
+#include <vector>
+#include <iostream>
+
 namespace sky{
   namespace core{
-    
-  }
+    public:
+      /**cxxflag**/
+      std::vector<std::string> m_cxxflag;
+      /**cflag**/
+      std::vector<std::string> m_cflag;
+      /**编译器**/
+      std::vector<std::string> m_compiler;
+      /**源码文件**/
+      std::vector<std::string> m_source;
+      /**头文件目录**/
+      std::vector<std::string> m_include;
+      /**链接库目录**/
+      std::vector<std::string> m_libpath;
+      /**链接库**/
+      std::vector<std::string> m_library;
+      /**设置安装目录**/
+      std::string              m_installpath;
+  };
 }
